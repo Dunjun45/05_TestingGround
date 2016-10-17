02c287940d071a1e2f12bf1ec9a255347f4ef1a7
Dunjun45 <dunjun45@gmail.com>
2016-10-17 23:11:17 +0800

    TG11 Converting Blueprint to C++

---
+++ Source/TestingGround/TP_ThirdPerson/ChooseNextWaypoint.cpp
@@ -0,0 +1,32 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "TestingGround.h"
+#include "ChooseNextWaypoint.h"
+#include "BehaviorTree/BlackboardComponent.h"
+#include "AIController.h"
+#include "PatrollingGuard.h"
+#include "Math.h"
+
+EBTNodeResult::Type UChooseNextWaypoint::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
+{
+	// Get the patrol points
+	auto AIController = OwnerComp.GetAIOwner();
+	auto ControlledPawn = AIController->GetPawn();
+	auto PatrollingGuard = Cast<APatrollingGuard>(ControlledPawn);
+	auto PatrolPoints = PatrollingGuard->PatrolPointsCPP;
+	
+	// Set next waypoint
+	auto BlackboardComp = OwnerComp.GetBlackboardComponent();
+	auto Index = BlackboardComp->GetValueAsInt(IndexKey.SelectedKeyName);
+	BlackboardComp->SetValueAsObject(WaypointKey.SelectedKeyName, PatrolPoints[Index]);
+
+	// Set next index
+	auto NextIndex = (Index + 1) % PatrolPoints.Num();
+	BlackboardComp->SetValueAsInt(IndexKey.SelectedKeyName, NextIndex);
+
+	//UE_LOG(LogTemp, Warning, TEXT("Waypoint index: %i"), Index);
+
+	
+	return EBTNodeResult::Succeeded;
+}
+
---
+++ Source/TestingGround/TP_ThirdPerson/ChooseNextWaypoint.h
@@ -0,0 +1,24 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "BehaviorTree/BTTaskNode.h"
+#include "ChooseNextWaypoint.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TESTINGGROUND_API UChooseNextWaypoint : public UBTTaskNode
+{
+	GENERATED_BODY()
+	
+	virtual EBTNodeResult::Type ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) override;
+	
+protected:
+	UPROPERTY(EditAnywhere, Category = "Blackbord")
+	struct FBlackboardKeySelector IndexKey;
+
+	UPROPERTY(EditAnywhere, Category = "Blackbord")
+	struct FBlackboardKeySelector WaypointKey;
+};
